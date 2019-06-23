d851bec422f1cf9df0b3286ba5ffbe48ea6298ba
Steven Xu <xhrsteven@gmail.com>
2019-06-23 16:21:22 +0800

    uPDATE

--- wikiSpider/wikiSpider/spiders/articleScrapy.py
+++ wikiSpider/wikiSpider/spiders/articleScrapy.py
@@ -1,9 +1,22 @@
-from scrapy.selector import Selector
 from scrapy import Spider
+from scrapy.spiders import Rule
+from scrapy.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.spiders import CrawlSpider
+
 from wikiSpider.items import Article
 
 
-class ArticleSpider(Spider):
+class ArticleSpider(CrawlSpider):
     name="article"
     allowed_domains = ["en.wikipedia.org"]
-    start_urls = ["http://"]
+    start_urls = ["http://en.wikipedia.org/wiki/Python_%28programming_language%29"]
+    rules = [Rule(SgmlLinkExtractor(allow=('(/wiki/)(?!:)*$'),),
+                                    callback="parse_item",follow=True)]
+
+    def parse(self, response):
+        item = Article()
+        title = response.xpath('//h1/text()')[0].extract()
+        print("Title is :" + title)
+        item['title'] = title
+        return item
+
