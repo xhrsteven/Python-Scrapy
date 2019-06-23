21512104dd399788c56c401a0d752953613165e4
Steven Xu <xhrsteven@gmail.com>
2019-06-23 11:43:55 +0800

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -8,7 +8,7 @@ def getTitle(url):
     except HTTPError as e:
         return None
     try:
-        bsObj = BeautifulSoup(html.read())
+        bsObj = BeautifulSoup(html.read(),features="lxml")
         title = bsObj.h1
     except AttributeError as e:
         return None
@@ -18,3 +18,10 @@ if title == None:
     print("Title could not be found")
 else:
     print(title)
+
+    
+html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
+bsObj = BeautifulSoup(html,features="lxml")
+nameList = bsObj.findAll("span",{"class": "green"})
+for name in nameList:
+    print(name.get_text())
