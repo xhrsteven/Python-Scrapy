0701c0f3e0c6c46df59dfcfcc963f5151fda0a50
Steven Xu <xhrsteven@gmail.com>
2019-06-23 16:29:41 +0800

    Update

--- wikiSpider/wikiSpider/spiders/articleScrapy.py
+++ wikiSpider/wikiSpider/spiders/articleScrapy.py
@@ -1,6 +1,6 @@
 from scrapy import Spider
 from scrapy.spiders import Rule
-from scrapy.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider
 
 from wikiSpider.items import Article
@@ -10,7 +10,7 @@ class ArticleSpider(CrawlSpider):
     name="article"
     allowed_domains = ["en.wikipedia.org"]
     start_urls = ["http://en.wikipedia.org/wiki/Python_%28programming_language%29"]
-    rules = [Rule(SgmlLinkExtractor(allow=('(/wiki/)(?!:)*$'),),
+    rules = [Rule(LinkExtractor(allow=('(/wiki/)(?!:)*$'),),
                                     callback="parse_item",follow=True)]
 
     def parse(self, response):
@@ -20,3 +20,5 @@ class ArticleSpider(CrawlSpider):
         item['title'] = title
         return item
 
+# scrapy crawl article -o articles.csv -t csv
+# scrapy crawl article -o articles.json -t json
